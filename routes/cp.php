060e108779c8ff9f5bfca4ec45c3c434a0990bbb
Camilo <cemart@gmail.com>
2022-08-16 10:33:29 -0500

    link para projecto conecta

--- routes/cp.php
+++ routes/cp.php
@@ -4,7 +4,8 @@ Route::group(['prefix' => 'memberbox'], function () {
     Route::get('', 'CP\MembersController@index')->name('memberbox.index');
     Route::get('create', 'CP\MembersController@create')->name('memberbox.create');
     Route::post('store', 'CP\MembersController@store')->name('memberbox.store');
-    Route::get('{user}', 'CP\MembersController@edit')->name('memberbox.edit');
+    //Cambio para projecto Conecta
+    Route::get('{user}', '\App\Http\Controllers\MembersController@edit')->name('edit');
     Route::patch('{user}', 'CP\MembersController@update')->name('memberbox.update');
     Route::delete('destroy', 'CP\MembersController@destroy')->name('memberbox.destroy');
     Route::get('export/{type}', 'CP\MembersController@export')->name('memberbox.export');
