4d078f09209cde12b80f5e1ab5a84e369415f313
Camilo <cemart@gmail.com>
2022-08-19 11:01:39 -0500

    cambiando para conecta actualizar

--- routes/cp.php
+++ routes/cp.php
@@ -4,9 +4,12 @@ Route::group(['prefix' => 'memberbox'], function () {
     Route::get('', 'CP\MembersController@index')->name('memberbox.index');
     Route::get('create', 'CP\MembersController@create')->name('memberbox.create');
     Route::post('store', 'CP\MembersController@store')->name('memberbox.store');
-    //Cambio para projecto Conecta
+    //Cambio Conecta Ver Usario
     Route::get('{user}', '\App\Http\Controllers\MembersController@edit')->name('edit');
-    Route::patch('{user}', 'CP\MembersController@update')->name('memberbox.update');
+
+    //Cambio Conecta Actualizar Usuario
+    Route::patch('{user}', '\App\Http\Controllers\MembersController@update')->name('memberbox.update');
+
     Route::delete('destroy', 'CP\MembersController@destroy')->name('memberbox.destroy');
     Route::get('export/{type}', 'CP\MembersController@export')->name('memberbox.export');
 });
